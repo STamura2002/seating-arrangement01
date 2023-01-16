9a841a18d9e64b9416b59349ad4ff0f83f96e988
STamura2002 <s.tamura020107@gmail.com>
2023-01-16 21:24:07 +0900

    08-4 解説3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,20 +9,20 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
-    
+
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    
+
     public function create()
     {
         return view('posts/create');
     }
-    
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
