3410ff513b618f8ccd332705544a5449bf4036ca
STamura2002 <s.tamura020107@gmail.com>
2023-01-16 22:08:57 +0900

    08-5 reproduction

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,29 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-         return $post->get();
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+
 }
